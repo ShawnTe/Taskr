9e6989e84f653ba61ca6cd1cda6c06616051aac1
shawn <shawn@naturalprofessional.com>
2019-01-04 19:34:06 -0800

    Add url helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def my_link_to(url)
+    # ^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$
+    url.gsub(/https?:\/\/w{0,3}\./, "")
+    # url.gsub(/trello/, "")
+  end
+
+  def limited_url(url)
+    /(.*)(?=\/)/.match(url)
+  end
 end
