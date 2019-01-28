31ae70a9afb14f422f06719375c59d73064598b2
shawn <shawn@naturalprofessional.com>
2019-01-27 21:17:29 -0800

    Add error handling on User login. Add required to username in User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,33 @@
 class UsersController < Clearance::UsersController
 
+  # def new
+  #   @user = user_from_params
+  #   p flash[:alert]
+  #   if flash[:alert]
+  #     flash.now[:alert]
+  #   end
+  #   render 'sessions/new'
+  # end
+
   def new
     @user = user_from_params
-    render 'sessions/new'
+    render template: "users/new"
   end
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      # flash[:messages] = @user.errors.messages
+      render template: "users/new"
+    end
+  end
+
+
+
 
   def show
     @user = current_user
@@ -37,7 +61,7 @@ private
   end
 
   def user_params
-    params.require(:user).permit(:username, :text_number, :email)
+    params.require(:user).permit(:username, :text_number, :email, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   include Clearance::User
 
+  validates :username, presence: true
+
   has_many :tasks, dependent: :destroy
 end
